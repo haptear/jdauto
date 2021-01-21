f39feda62814b46ae83d724fed87e98df5c3d2dd
haptear <haptear@hotmail.com>
2021-01-21 18:11:14 +0800

    更新

--- clear.bat
+++ clear.bat
@@ -2,3 +2,6 @@ rem fart -r --remove *.js "\"undefined\"!=typeof"
 rem fart -r --remove *.js "process&&JSON.stringify(process.env).indexOf(\"GITHUB\")>-1&&process.exit(0);"
 
 fnr --cl --dir .\ --fileMask  "*.js"  --includeSubDirectories --find "\"undefined\"!=typeof process&&JSON.stringify(process.env).indexOf(\"GITHUB\")>-1&&process.exit(0);" --replace ""
+
+fnr --cl --dir .\ --fileMask  "*.js"  --includeSubDirectories --find "if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)" --replace ""
+
