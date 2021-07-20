17fe9f6b67b113889afdd031ce5641414d3ad007
haptear <haptear@hotmail.com>
2021-07-20 17:08:11 +0800

    1

--- run.bat
+++ run.bat
@@ -1,10 +1,16 @@
 rem 先把.github目录设置未隐藏，这样xcopy就不会拷贝.git 和 .github目录
 rem git pull
 del jdCookie.js
+
+rem 编译ts
+
 xcopy *  ..\jdauto\  /E /Y
 
 cd ..\jdauto
+call tsc --build tsconfig.json
 
+cd ..\genyml
+call run.bat
+
+cd ..\jdauto
 clear.bat
-
-cd ..\jd_scripts_new
