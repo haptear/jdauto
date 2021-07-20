94890b8e684d5a456d48fb15f47064a0780163f4
haptear <haptear@hotmail.com>
2021-07-20 18:32:04 +0800

    d4

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -27,7 +27,8 @@ cron "10 * * * *" script-path=jd_jdfactory.js,tag=东东工厂
 const $ = new Env('东东工厂');
 
 console.log('\n====================Hello World====================\n')
-
+console.log('1\n')
+console.log(`2 ${$.isNode()}\n`)
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
